da4c57ed53e601588bcecfc13d40f0f426857d4d
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2024-04-23 06:34:46 +0000

    add cdk

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set alias
+echo 'alias ll="ls -alF"' >> ~/.bash_aliases
+echo 'alias g="git"' >> ~/.bash_aliases
+echo 'alias d="docker"' >> ~/.bash_aliases
+echo 'alias dc="docker compose"' >> ~/.bash_aliases
+echo 'alias sso="source /workspaces/nextgen_payroll_infra/aws_connect.sh"' >> ~/.bash_aliases
+
+# setup volta
+volta setup
+volta pin pnpm@latest
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceOutput = new codepipeline.Artifact();
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineType: codepipeline.PipelineType.V2,
+      pipelineName: 'TestPipeline',
+      crossAccountKeys: false,
+      enableKeyRotation: false,
+    });
+
+    // GitHub接続情報取得
+    const sourceAction = new codepipeline_actions.CodeStarConnectionsSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'kaito01234',
+      repo: 'github-branching-strategy',
+      branch: 'main',
+      output: sourceOutput,
+      triggerOnPush: false,
+      connectionArn: `arn:aws:codestar-connections:ap-northeast-1:948669373988:connection/868491e5-ad8b-4ec1-bdb3-43b676d9021b`,
+    });
+
+    const approvalAction = new codepipeline_actions.ManualApprovalAction({
+      actionName: 'ApprovalAction',
+  });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [sourceAction],
+    });
+
+    pipeline.addStage({
+      stageName: 'Approval',
+      actions: [approvalAction],
+    });
+
+  }
+}
